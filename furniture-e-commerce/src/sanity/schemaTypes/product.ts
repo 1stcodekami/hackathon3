65b1e4cd9fe026c022d22d30616c138f53ea49fc
1stcodekami <mkkhurshid33@gmail.com>
2025-01-24 17:31:07 +0500

    update

--- furniture-e-commerce/src/sanity/schemaTypes/product.ts
+++ furniture-e-commerce/src/sanity/schemaTypes/product.ts
@@ -1,58 +1,59 @@
-// import { defineType } from "sanity";
+import { defineType } from 'sanity';
 
-export default {
-    name: 'product',
-    title: 'Product',
-    type: 'document',
-    fields: [
-      {
-        name: 'id',
-        title: 'ID',
-        type: 'string',
+const productSchema = defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'id',
+      title: 'ID',
+      type: 'string',
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
       },
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: {
-          hotspot: true, // Enable image cropping
-        },
-      },
-      {
-        name: 'price',
-        title: 'Price',
-        type: 'number',
-      },
-      {
-        name: 'description',
-        title: 'Description',
-        type: 'text',
-      },
-      {
-        name: 'discountPercentage',
-        title: 'Discount Percentage',
-        type: 'number',
-      },
-      {
-        name: 'isFeaturedProduct',
-        title: 'Is Featured Product',
-        type: 'boolean',
-      },
-      {
-        name: 'stockLevel',
-        title: 'Stock Level',
-        type: 'number',
-      },
-      {
-        name: 'category',
-        title: 'Category',
-        type: 'string',
-      },
-    ],
-  };
-  
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'discountPercentage',
+      title: 'Discount Percentage',
+      type: 'number',
+    },
+    {
+      name: 'isFeaturedProduct',
+      title: 'Is Featured Product',
+      type: 'boolean',
+    },
+    {
+      name: 'stockLevel',
+      title: 'Stock Level',
+      type: 'number',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+  ],
+});
+
+export default productSchema;
